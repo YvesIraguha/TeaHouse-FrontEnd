202414347297dace66d23da24b30bd240728c9d5
YvesIraguha <iraguhaivos@gmail.com>
2020-02-12 17:53:31 +0200

    Updates

---
+++ precache-manifest.c7cc59ae9256d2c8f2503d51390e504a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d07ff3295cca880527901c2df58178",
+    "url": "/TeaHouse-FrontEnd/index.html"
+  },
+  {
+    "revision": "c14ef380bdc443eb43c7",
+    "url": "/TeaHouse-FrontEnd/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "83abec2f94c24bba6577",
+    "url": "/TeaHouse-FrontEnd/static/css/main.868dc930.chunk.css"
+  },
+  {
+    "revision": "c14ef380bdc443eb43c7",
+    "url": "/TeaHouse-FrontEnd/static/js/2.f9e00ca4.chunk.js"
+  },
+  {
+    "revision": "2933bc7440003894c73e754566123cfe",
+    "url": "/TeaHouse-FrontEnd/static/js/2.f9e00ca4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83abec2f94c24bba6577",
+    "url": "/TeaHouse-FrontEnd/static/js/main.717860d3.chunk.js"
+  },
+  {
+    "revision": "a5054e8471aefede2e7c",
+    "url": "/TeaHouse-FrontEnd/static/js/runtime-main.b9de58d6.js"
+  },
+  {
+    "revision": "17f5a986dc95edab882beffa893bfdd5",
+    "url": "/TeaHouse-FrontEnd/static/media/books-stack-of-three.17f5a986.svg"
+  },
+  {
+    "revision": "51c44790b31f99759ffa25c30cca8687",
+    "url": "/TeaHouse-FrontEnd/static/media/email.51c44790.svg"
+  },
+  {
+    "revision": "67c465e116edf3072532520df809cebb",
+    "url": "/TeaHouse-FrontEnd/static/media/folder.67c465e1.svg"
+  },
+  {
+    "revision": "6013bfe87413f0a919a0f0f8fad90435",
+    "url": "/TeaHouse-FrontEnd/static/media/hamburger_icon6.6013bfe8.svg"
+  },
+  {
+    "revision": "50bc3871c56b34f2f2c64e868982f5ce",
+    "url": "/TeaHouse-FrontEnd/static/media/location.50bc3871.svg"
+  },
+  {
+    "revision": "f25cdf1d9ffe9b11a720c351f75a4ca7",
+    "url": "/TeaHouse-FrontEnd/static/media/parchment1.f25cdf1d.svg"
+  },
+  {
+    "revision": "f04da1e82b017d970176fee5fa7784c7",
+    "url": "/TeaHouse-FrontEnd/static/media/pencil.f04da1e8.svg"
+  },
+  {
+    "revision": "9e1788e93d1dfd56968b6e5c65721bab",
+    "url": "/TeaHouse-FrontEnd/static/media/phone-call.9e1788e9.svg"
+  },
+  {
+    "revision": "c65f6a86d450fc4507e91459539e36da",
+    "url": "/TeaHouse-FrontEnd/static/media/play.c65f6a86.svg"
+  },
+  {
+    "revision": "2cef3e940ade1acaee7b7c9c8544b888",
+    "url": "/TeaHouse-FrontEnd/static/media/reading.2cef3e94.svg"
+  },
+  {
+    "revision": "abe3b8d7e01122c323e7cc4db17be68f",
+    "url": "/TeaHouse-FrontEnd/static/media/rules.abe3b8d7.svg"
+  },
+  {
+    "revision": "548230907631cbcc675e0f06e3b05791",
+    "url": "/TeaHouse-FrontEnd/static/media/scroll.54823090.svg"
+  },
+  {
+    "revision": "06808a74055ef64da470863533a8f999",
+    "url": "/TeaHouse-FrontEnd/static/media/short.06808a74.svg"
+  },
+  {
+    "revision": "7a7d00bb83f94df3addd65bb4cbadc14",
+    "url": "/TeaHouse-FrontEnd/static/media/submissions.7a7d00bb.svg"
+  }
+]);
