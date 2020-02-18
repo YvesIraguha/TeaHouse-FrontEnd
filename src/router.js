5606314cc97e3ad4d828376cac4edeb5c07fdb0d
YvesIraguha <iraguhaivos@gmail.com>
2020-02-18 15:45:24 +0200

    change to HashRouter to work on GitHub

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./Common/NavBar";
 import Footer from "./Common/Footer";
 import Home from "./Home";
