ad0a56ac2ad63258b4a62540cbdb7bcb6b249036
Yves Iraguha <34691070+YvesIraguha@users.noreply.github.com>
2020-08-27 12:27:33 +0200

    Chore/change fav icon (#25)
    * feature(feedback): apply feedback
    
    * add fav icon in the title
    
    * refactor submissions

--- src/DirectionsPage/index.js
+++ src/DirectionsPage/index.js
@@ -38,15 +38,18 @@ export default () => (
     <p>
       This is a section for gossip! It captures interesting news from the
       mainstream, boils it in a kettle, and serves it in mugs like tea. Welcome
-      to the tea house! [Translation: These will be blog posts in the form of
-      short plays. We will pick up interesting stories from around the Rwandan
-      mainstream, and imagine them in a fictionalized way, then create out of
-      them our own stories. With the photographic law that in public there is no
-      privacy, we may decide to use in our plays the original names of the
-      individuals involved in the news stories we pick, or mention the news
-      story and then create our own fictionalized names. The series will be run
-      by the editor or anyone who is interested, who, if so, should submit their
-      play via the usual submissions form.]
+      to the tea house!
+    </p>
+    <p>
+      [Translation: These will be blog posts in the form of short plays. We will
+      pick up interesting stories from around the Rwandan mainstream, and
+      imagine them in a fictionalized way, then create out of them our own
+      stories. With the photographic law that in public there is no privacy, we
+      may decide to use in our plays the original names of the individuals
+      involved in the news stories we pick, or mention the news story and then
+      create our own fictionalized names. The series will be run by the editor
+      or anyone who is interested, who, if so, should submit their play via the
+      usual submissions form.]
     </p>
   </div>
 );
